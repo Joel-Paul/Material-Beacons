8bbb1b1e055ef351b1dca890c4bf8cad89c9effd
Joel Paul <joel.v.paul@outlook.com>
2024-05-15 22:19:26 +1000

    Update to 1.20

--- src/main/java/issame/material_beacons/MaterialBeacons.java
+++ src/main/java/issame/material_beacons/MaterialBeacons.java
@@ -1,37 +1,23 @@
 package issame.material_beacons;
 
-import issame.material_beacons.block.entity.MBBlockEntity;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.resource.ResourceManager;
-import net.minecraft.resource.ResourceType;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.InputStream;
+import net.minecraft.util.ActionResult;
 
 public class MaterialBeacons implements ModInitializer {
-	public static final String MODID = "material_beacons";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Fabric world!");
+        disableBeaconGUI();
+    }
 
-	public static BlockEntityType<MBBlockEntity> MATERIAL_BEACON_BLOCK_ENTITY;
-
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Initialising");
-
-		MATERIAL_BEACON_BLOCK_ENTITY = Registry.register(
-				Registry.BLOCK_ENTITY_TYPE,
-				new Identifier(MODID, "beacon_block_entity"),
-				FabricBlockEntityTypeBuilder.create(MBBlockEntity::new, Blocks.BEACON).build()
-		);
-
-		ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new MBResourceReloadListener());
-	}
+    private void disableBeaconGUI() {
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            if (world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.BEACON) {
+                return ActionResult.FAIL;
+            }
+            return ActionResult.PASS;
+        });
+    }
 }
