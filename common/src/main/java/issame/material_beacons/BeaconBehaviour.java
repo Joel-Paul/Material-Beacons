f1da1706a1a8a5f7dd033c27034a5c0f8ac5b3f8
Joel Paul <joel.v.paul@outlook.com>
2026-09-22 22:33:47 +1000

    Refactor code

--- common/src/main/java/issame/material_beacons/BeaconBehaviour.java
+++ common/src/main/java/issame/material_beacons/BeaconBehaviour.java
@@ -1,7 +1,6 @@
 package issame.material_beacons;
 
 import issame.material_beacons.config.BeaconData;
-import issame.material_beacons.config.BlockOrTag;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -9,77 +8,64 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.phys.AABB;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.LinkedList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 import static issame.material_beacons.Constants.MAX_LAYER;
 
 public class BeaconBehaviour {
-    public static int updateBeaconBase(Level level, int x, int y, int z, Map<Identifier, BeaconData> beaconData) {
-        BlockPos pos = new BlockPos(x, y, z);
-        Block below = level.getBlockState(pos.below()).getBlock();
-        List<BeaconData> bases = matchBases(below, beaconData);
-        if (bases == null) {
+    public static int updateBeaconBase(Level level, int x, int y, int z, Map<Identifier, BeaconData> beaconDataMap) {
+        BlockPos beaconPos = new BlockPos(x, y, z);
+        Block below = level.getBlockState(beaconPos.below()).getBlock();
+        List<BeaconData> beaconDataList = filterBeaconData(below, beaconDataMap.values());
+        if (beaconDataList.isEmpty()) {
             return 0;
         }
 
-        // Verify that the beacon pyramid is using a valid base
-        // Loops in a pyramid shape from top to bottom
-        int currentLayer = 0;
+        int maxLayer = matchBeaconMaterial(level, x, y, z, beaconDataList);
+        applyEffects(level, beaconPos, maxLayer, beaconDataList.getFirst());
+
+        return maxLayer;
+    }
+
+    private static List<BeaconData> filterBeaconData(Block block, Collection<BeaconData> beaconDataCollection) {
+        return beaconDataCollection.stream().filter(
+                base -> base.getBases().stream().anyMatch(blockOrTag -> blockOrTag.has(block))
+        ).toList();
+    }
+
+    private static int matchBeaconMaterial(Level level, int x, int y, int z, List<BeaconData> beaconDataList) {
+        int maxLayer = 0;
         for (int layer = 1; layer <= MAX_LAYER; layer++) {
             if (y - layer < level.getMinY()) {
                 break;
             }
             // Loops in a square shape for each layer
-            List<BeaconData> nextBases = bases;
-            for (int i = x - layer; (i <= x + layer) && !nextBases.isEmpty(); i++) {
-                for (int j = z - layer; (j <= z + layer) && !nextBases.isEmpty(); j++) {
+            List<BeaconData> newBases = beaconDataList;
+            for (int i = x - layer; (i <= x + layer) && !newBases.isEmpty(); i++) {
+                for (int j = z - layer; (j <= z + layer) && !newBases.isEmpty(); j++) {
                     Block block = level.getBlockState(new BlockPos(i, y - layer, j)).getBlock();
-                    nextBases = bases.stream()
-                            .filter(data -> data.getBases().stream()
-                                    .anyMatch(blockOrTag -> blockOrTag.has(block)))
-                            .toList();
+                    newBases = filterBeaconData(block, newBases);
                 }
             }
-            if (nextBases.isEmpty()) {
+            if (newBases.isEmpty()) {
                 break;
             }
-            currentLayer = layer;
-            bases = nextBases;
+            maxLayer = layer;
+            beaconDataList = newBases;
         }
-
-        if (currentLayer > 0 && !bases.isEmpty()) {
-            // Apply the effects of a beacon base.
-            BeaconData base = bases.getFirst();
-            applyEffects(level, pos, currentLayer, base);
-        }
-
-        return currentLayer;
+        return maxLayer;
     }
 
-    @Nullable
-    private static List<BeaconData> matchBases(Block block, Map<Identifier, BeaconData> beaconData) {
-        List<BeaconData> matching = new LinkedList<>();
-        for (BeaconData data : beaconData.values()) {
-            for (BlockOrTag blockOrTag : data.getBases()) {
-                if (blockOrTag.has(block)) {
-                    matching.add(data);
-                }
-            }
-        }
-        return matching.isEmpty() ? null : matching;
-    }
-
-    private static void applyEffects(Level level, BlockPos pos, int layer, BeaconData base) {
-        if (level.isClientSide() || base.getAllPowers().isEmpty()) {
+    private static void applyEffects(Level level, BlockPos pos, int layer, BeaconData beaconData) {
+        if (level.isClientSide() || layer <= 0 || beaconData.getAllPowers().isEmpty()) {
             return;
         }
 
-        List<MobEffectInstance> effects = base.getPowers(layer);
-        List<Double> ranges = base.getRanges(layer);
+        List<MobEffectInstance> effects = beaconData.getPowers(layer);
+        List<Double> ranges = beaconData.getRanges(layer);
 
         for (int i = 0; i < effects.size(); i++) {
             MobEffectInstance effect = effects.get(i);
